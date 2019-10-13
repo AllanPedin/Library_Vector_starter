c297ecec2ec55351046cb532d3d32f5aad4e9ac5
Allan Pedin <allan.pedin.17@cnu.edu>
2019-10-13 06:01:12 -0400

    load books done in theory

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -1,5 +1,28 @@
 #include "../includes_usr/fileIO.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
+
+book parseBook(std::string line){
+	book myBook;
+	stringstream streamLine(line);
+	string token;
+	for(int i=0;getline(streamLine, token, ',');i++){
+		switch(i){
+			case 0:myBook.book_id=stoi(token);break;
+			case 1:myBook.title=token;break;
+			case 2:myBook.author=token;break;
+			case 3:if(stoi(token)==1){
+				myBook.state= IN;
+			}break;//might need if for OUT but i dont know what that looks like yet//TODO
+			case 4:myBook.loaned_to_patron_id=stoi(token);break;
+			default:cout<<"pls dont go here \n";break;
+		}
+	}
+	return myBook;
+}
 /* clears, then loads books from the file filename
  * returns  COULD_NOT_OPEN_FILE if cannot open filename
  * 			NO_BOOKS_IN_LIBRARY if there are 0 entries in books
@@ -7,6 +30,16 @@ using namespace std;
  * */
 int loadBooks(std::vector<book> &books, const char* filename)
 {
+	ifstream fileStream (filename);
+	if(!fileStream.is_open()){
+		return COULD_NOT_OPEN_FILE;
+	}
+	for (string line; getline(fileStream,line); ){
+		books.push_back(parseBook(line));
+	}
+	if(books.size()==0){
+		return NO_BOOKS_IN_LIBRARY;
+	}
 	return SUCCESS;
 }
 
